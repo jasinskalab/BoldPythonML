b825063d8ce6878fff84798f07215516fd06b72d
mato9020 <matteson@udel.edu>
2020-04-08 17:16:47 -0400

    literacy window changed

--- Model/model_with_input.py
+++ Model/model_with_input.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 import tensorflow as tf
 from tensorflow.keras import layers
 import csv
@@ -9,9 +12,7 @@ from scipy.io import loadmat
 #from tensorflow.data import Dataset
 import sys
 from sklearn.model_selection import KFold
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+
 
 from datetime import datetime
 import io
